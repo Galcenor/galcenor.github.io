307b1043a9932f76651a2c31de0d6718b543a7d0
Lenny <lemairelenny02@gmail.com>
2023-10-18 15:49:43 +0200

    Adding security for the empty input

--- assets/script.js
+++ assets/script.js
@@ -2,24 +2,28 @@ $(document).ready(function() {
     // Générer un nombre aléatoire entre 0 et 100
     const randomNumber = Math.floor(Math.random() * 101);
 
-    $('#playButton').click(function(event) {
+    $('#playButton').click(function() {
+        const userGuess = $('#numberInput').val().trim(); // Je récupère la valeur entrée
 
-        const userGuess = parseInt($('#numberInput').val()); // Je récupère la valeur entrée
-
-        if (isNaN(userGuess)) {
+        if (userGuess === '') {
+            $('#msg').text('Merci de remplir le champ vide');
+            $('#msg').css('color', 'red');
+        } else if (isNaN(userGuess)) {
             $('#msg').text('Merci d\'utiliser un nombre !');
             $('#msg').css('color', 'red');
-        }
+        } else {
+            const userGuessNumber = parseInt(userGuess); // Convertir en nombre
 
-        if (userGuess === randomNumber) { // Verification si les deux nombres sont correspondants
-            $('#msg').text('Félicitations ! Vous avez deviné le nombre');
-            $('#msg').css('color', 'green');
-        } else if (userGuess < randomNumber) { // si le nombre du joueur est plus petit que le nombre générer
-            $('#msg').text('Le nombre est plus grand. Essayez à nouveau.');
-            $('#msg').css('color', 'lightcoral');
-        } else if(userGuess > randomNumber) { // Sinon le nombre est plus grand
-            $('#msg').text('Le nombre est plus petit. Essayez à nouveau.');
-            $('#msg').css('color', 'lightcoral');
+            if (userGuessNumber === randomNumber) { // Vérification si les deux nombres correspondent
+                $('#msg').text('Félicitations ! Vous avez deviné le nombre');
+                $('#msg').css('color', 'green');
+            } else if (userGuessNumber < randomNumber) { // si le nombre du joueur est plus petit que le nombre généré
+                $('#msg').text('Le nombre est plus grand. Essayez à nouveau.');
+                $('#msg').css('color', 'lightcoral');
+            } else if (userGuessNumber > randomNumber) { // Sinon le nombre est plus grand
+                $('#msg').text('Le nombre est plus petit. Essayez à nouveau.');
+                $('#msg').css('color', 'lightcoral');
+            }
         }
     });
 });
